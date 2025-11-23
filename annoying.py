4efdde8c9602f71e04e7551154a611940539be78
Paragraph1148 <actuallyamountain@proton.me>
2025-11-23 07:35:40 +0530

    isX dot methods

--- annoying.py
+++ annoying.py
@@ -1,3 +1,12 @@
-spam = {'bar': 100}
-print(spam['bar'])
-print(spam.keys())
+while True:
+    print('Enter your age: ')
+    age = input()
+    if age.isdecimal():
+        break
+    print('Please enter a valid number.')
+while True:
+    print('Enter new password: ')
+    password = input()
+    if password.isalnum():
+        break
+    print('Please enter alphanumeric only.')
